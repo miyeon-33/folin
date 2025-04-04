75449dfb82a9956bef51f624df688f646c3683b3
Zoeryeon <gpfus3869@gmail.com>
2025-04-04 14:52:03 +0900

    merge 충돌해결

--- src/components/Sidemenu.jsx
+++ src/components/Sidemenu.jsx
@@ -1,3 +1,26 @@
+import { Link } from 'react-router';
+
+const menus = [
+  { path: '/series', menu: '시리즈' },
+  { path: '/seminar', menu: '세미나' },
+  { path: '/talk', menu: '톡' },
+];
+
 export default function Sidemenu() {
-  return <div>Sidemenu</div>;
+  return (
+    <div className="relative flex items-center ">
+      <ul className=" flex gap-[30px]">
+        {menus.map((item) => (
+          <li key={item.menu} className=" flex flex-col">
+            <Link
+              to={item.path}
+              className="text-[18px] text-[#111] font-semiboldbold"
+            >
+              {item.menu}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
