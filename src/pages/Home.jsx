b981d4bbc883cf93bea902d571331a3d066c282d
miyeon-33 <elav0303@gmail.com>
2025-04-07 16:09:31 +0900

    feat: 헤더, 시리즈섹션 1 완성 #19

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import NewSeries from '@/components/NewSeries';
 import Subscribe from '@/components/Subscribe';
 
 export default function Home() {
   return (
-    <main className="h-[1000px] mt-[56px]">
+    <main className="h-[1000px] mt-[56px] max-sm:mt-[60px] bg-[#ddd]">
       <Subscribe />
+      <NewSeries />
     </main>
   );
 }
