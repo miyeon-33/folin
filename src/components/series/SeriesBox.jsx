9c934acd5d8090f62d3b6ffa4d5ab89324f5d35d
Zoeryeon <gpfus3869@gmail.com>
2025-04-07 18:03:45 +0900

    feat: 시리즈 중간 저장0407

--- src/components/series/SeriesBox.jsx
+++ src/components/series/SeriesBox.jsx
@@ -3,13 +3,15 @@ import arrow from '@/assets/images/rhr/arrow.png';
 import arrowG from '@/assets/images/rhr/arrow_g.png';
 import newBtn from '@/assets/images/icon/newBtn.svg';
 import img1 from '@/assets/images/rhr/1743333644564_a_id-m2.jpg';
+import slideOffL from '@/assets/images/rhr/slideOffL.png';
+import slideOffR from '@/assets/images/rhr/slideOffR.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import { useState } from 'react';
 import { Link } from 'react-router';
+import styles from './SeriesBox.module.css';
 
 export default function SeriesBox() {
   const [isToggle, setIsToggle] = useState(true);
@@ -32,60 +34,52 @@ export default function SeriesBox() {
         </Link>
       </div>
 
-      <div className="pt-[16px] w-[calc(33.3333% - 16px)]">
+      <div>
         <Swiper
-          modules={[Navigation, Pagination]}
+          modules={[Navigation]}
           spaceBetween={24}
           slidesPerView={3}
           navigation
           onSlideChange={() => console.log('slide change')}
           onSwiper={(swiper) => console.log(swiper)}
-          className=""
+          className={styles.slider}
         >
           <SwiperSlide className="">
-            <Link className="">
-              <div className="hover:translate-y-[-16px] transition-transform duration-300">
-                <div className="h-[auto] w-[calc(100% - 16px)] aspect-4/3 border">
-                  <div className="absolute z-2 top-[10px] left-[10px]">
-                    <img
-                      src={newBtn}
-                      alt="새로운게시물"
-                      className="object-cover"
-                    />
+            <Link className="block transition-all  text-[#111] hover:text-point1 hover:-translate-y-[16px]">
+              <div className="">
+                <img
+                  src={newBtn}
+                  alt="새로운게시물"
+                  className="absolute z-2 top-[10px] left-[10px]"
+                />
+                <img
+                  src={img1}
+                  alt="이미지1"
+                  className="w-full h-full object-cover rounded-[6px]"
+                />
+              </div>
+              <div className="relative w-[calc(100% - 16px)] h-[auto] p-[10px] top-[-15px] right-[-15px] bg-white rounded-[6px]">
+                <div className="flex items-center gap-[2px]">
+                  <div
+                    className="max-w-[calc(100% - 35px)] bg-[#F2EC72] border border-[#F2EC72] rounded-[6px] py-[6px] px-[8px]
+                    text-[#111] text-[12px] font-bold"
+                  >
+                    1등 브랜드의 비밀
                   </div>
-                  <div className="relative min-w-[120px] h-full">
-                    <div className="absolute w-full h-full">
-                      <img
-                        src={img1}
-                        alt="이미지1"
-                        className="w-full h-full object-cover rounded-[6px]"
-                      />
-                    </div>
+                  <div
+                    className="inline-block bg-white border border-[#F2EC72] rounded-[6px] py-[6px] px-[8px]
+                    text-[#111] text-[12px] font-bold"
+                  >
+                    2화
                   </div>
                 </div>
-                <div className="relative w-[calc(100% - 16px)] h-[auto] p-[10px] top-[-15px] right-[-15px] bg-white rounded-[6px]">
-                  <div className="flex items-center gap-[2px]">
-                    <div
-                      className="max-w-[calc(100% - 35px)] bg-[#F2EC72] border border-[#F2EC72] rounded-[6px] py-[6px] px-[8px]
-                    text-[#111] text-[12px] font-bold"
-                    >
-                      1등 브랜드의 비밀
-                    </div>
-                    <div
-                      className="inline-block bg-white border border-[#F2EC72] rounded-[6px] py-[6px] px-[8px]
-                    text-[#111] text-[12px] font-bold"
-                    >
-                      2화
-                    </div>
-                  </div>
-                  <p className="line-clamp-2 break-words overflow-ellipsis mt-[10px] mb-[16px] text-[18px] font-bold text-[#111] leading-[130%]">
-                    동서식품② 시장점유율 90%, 맥심이 45년간 정상을 지킨 법
-                  </p>
-                  <div className="overflow-ellipsis whitespace-nowrap overflow-hidden">
-                    <span className=" text-[12px] font-bold text-[#111] leading-[150%] mr-[8px]">
-                      하치수
-                    </span>
-                  </div>
+                <p className="line-clamp-2 break-words overflow-ellipsis mt-[10px] mb-[16px] text-[18px] font-bold leading-[130%]">
+                  동서식품② 시장점유율 90%, 맥심이 45년간 정상을 지킨 법
+                </p>
+                <div className="overflow-ellipsis whitespace-nowrap overflow-hidden">
+                  <span className=" text-[12px] font-bold leading-[150%] mr-[8px]">
+                    하치수
+                  </span>
                 </div>
               </div>
             </Link>
