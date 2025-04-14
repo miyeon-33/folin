194bb2358057e82879fb9b213e9b4a85d1f48a89
hbang012 <hbang4888@gmail.com>
2025-04-14 13:04:46 +0900

    feat:세미나 페이지 추가 #62
    ```

--- src/pages/SeminarDetails.jsx
+++ src/pages/SeminarDetails.jsx
@@ -1,8 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router';
+
 export default function SeminarDetails() {
+  const { id } = useParams();
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['seminar', id],
+    queryFn: () => fetch(`/api/seminars/${id}`).then((res) => res.json()),
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+  const seminarData = {};
+
   return (
     <main className="bg-[#ebedec] ">
       <div className="max-w-[1200px] h-[1500px] mx-auto flex justify-center">
-        <ul className="mt-[50px]"></ul>
+        <ul className="mt-[50px]">
+          <h1>{data.dettit}</h1>
+          <img src={data.detimg} alt={data.dettit} />
+          <p>....</p>
+        </ul>
       </div>
     </main>
   );
