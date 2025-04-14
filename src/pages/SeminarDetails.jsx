db67a2f94e1d7275b769571566af37258cd769f5
hbang012 <hbang4888@gmail.com>
2025-04-14 17:36:36 +0900

    feat: 아티클데이터 추가 및 세미나 업데이트 #70
    ```

--- src/pages/SeminarDetails.jsx
+++ src/pages/SeminarDetails.jsx
@@ -3,7 +3,6 @@ import { useParams } from 'react-router';
 
 export default function SeminarDetails() {
   const { articleId } = useParams();
-
   const { isLoading, data, isError, error } = useQuery({
     queryKey: ['seminar', articleId],
     queryFn: () =>
@@ -14,16 +13,59 @@ export default function SeminarDetails() {
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
 
-  const seminarData = {};
-
   return (
     <main className="bg-[#ebedec] ">
-      <div className="max-w-[1200px] h-[1500px] mx-auto flex justify-center">
-        <ul className="mt-[50px]">
-          <h1>{data.dettit}</h1>
-          <img src={data.detimg} alt={data.dettit} />
+      <div className="max-w-[1200px] h-[1500px] mx-auto flex justify-center ">
+        <div className="mt-[102px]">
+          <div className="m-[0px_auto_40px] max-w-[256px] ">
+            <img
+              className="rounded-[6px] h-[342px] w-[full] "
+              src={data?.seminar?.image}
+              alt={data?.seminar?.title}
+            />
+          </div>
+          <div className=""></div>
+          <div className="text-center">
+            <h3 className="mb-[8px] text-[13px] text-[#00aa73] font-medium">
+              {data?.details?.type?.map((item) => (
+                <span key={item} className="mr-[10px]">
+                  {item}
+                </span>
+              ))}
+            </h3>
+            <h2 className="text-[28px] font-extrabold">
+              {data?.details?.dettit}
+            </h2>
+          </div>
+          <div className="flex justify-center gap-[18px] m-[32px_auto_40px]">
+            <img
+              src={data?.profile?.pfpImg}
+              alt={data?.profile?.pfpName}
+              className="rounded-[50%] w-[34px] h-[34px] "
+            />
+            <p className="leading-[32px] font-medium">
+              <strong className="font-extrabold mr-[8px]">
+                {data?.profile?.pfpName}
+              </strong>
+              {data?.profile?.pfpJob}
+            </p>
+          </div>
+          <div className="mx-auto bg-white rounded-[10px] p-[16px_16px_10px] w-[100%] max-w-[384px] h-[135px]">
+            <p className="flex justify-between font-medium mb-[8px]">
+              <strong className="font-bold">개별 구매가</strong>
+              ....원
+            </p>
+            <p className="flex justify-between text-[#00aa73]">
+              <strong className="font-bold leading-[42px]">
+                Plus 멤버십 회원가
+              </strong>
+              <span>
+                <em className="font-extrabold text-[24px]">....</em>원
+              </span>
+            </p>
+          </div>
           <p>....</p>
-        </ul>
+        </div>
       </div>
     </main>
   );
