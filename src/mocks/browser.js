c81ae0bb8249094736725ec3462103f06a07c8cc
miyeon-33 <elav0303@gmail.com>
2025-04-16 14:44:32 +0900

    feat: 링커핸들러추가 #90

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -2,10 +2,14 @@ import { setupWorker } from 'msw/browser';
 import { seriesHandlers } from './seriesHandlers.js';
 import { articleHandlers } from '@/mocks/articleHandler.js';
 import { seminarHandlers } from '@/mocks/seminarHandler.js';
+import { linkerHandlers } from '@/mocks/linkerHandlers.js';
+import { plannerHandler } from '@/mocks/plannerHandler.js';
 
 // 배열의 요청 핸들러 함수들을 전개연산자로 전달하여 실행
 export const worker = setupWorker(
   ...seriesHandlers,
   ...articleHandlers,
-  ...seminarHandlers
+  ...seminarHandlers,
+  ...linkerHandlers,
+  ...plannerHandler
 );
