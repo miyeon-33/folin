b1e745d66a531cc913f0d56950bbf96528b64fcc
miyeon-33 <elav0303@gmail.com>
2025-04-21 16:36:12 +0900

    feat: 서치핸들러 #115

---
+++ src/components/search/Tab.jsx
@@ -0,0 +1,46 @@
+import Results from '@/components/search/Results';
+import SeriesSearch from '@/components/search/SeriesSearch';
+import { useState } from 'react';
+
+const initTabs = [
+  { menu: '전체', contents: '' },
+  { menu: '시리즈', contents: '' },
+  { menu: '아티클', contents: '' },
+  { menu: '비디오', contents: '' },
+  { menu: '세미나', contents: '' },
+  { menu: '링커', contents: '' },
+];
+
+export default function Tab({ data = [] }) {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const flattenedData = Array.isArray(data) ? data.flat() : [];
+  const filteredData = flattenedData.filter((item) => item.video === 'Y');
+
+  return (
+    <div className="max-sm:top-[8px] px-[24px] max-md:px-[24px] mb-[72px]">
+      <div className="max-w-[1200px] mx-auto">
+        <div className="h-[22px] mb-[64px]">
+          <ul className="flex items-center font-medium">
+            {initTabs.map((tab, index) => (
+              <button
+                key={index}
+                className={`pr-[12px] break-keep after:h-[16px] last:after:border-r-0 after:border-r after:ml-[12px]
+          after:border-[#bfbfbf] flex gap-[2px] ${
+            index === activeTab ? 'text-point1' : 'text-gray-600'
+          }`}
+                onClick={() => setActiveTab(index)}
+              >
+                {tab.menu}
+                {tab.menu === '시리즈' && <span>{data.length}</span>}
+                {tab.menu === '아티클' && <span>{data.length}</span>}
+                {tab.menu === '비디오' && <span>{filteredData.length}</span>}
+              </button>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div>{initTabs[activeTab].component}</div>
+    </div>
+  );
+}
